3e9a0834fb65edc0521b080f95f8b019351a475a
trollord <80673827+trollord@users.noreply.github.com>
2022-11-04 15:08:13 +0530

    Update socket.java

--- socket.java
+++ socket.java
@@ -1,1 +1,36 @@
+import java.io.*;
+import java.net.*;
 
+public class Myclient {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("localhost", 6666);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("Hello Serveer");
+            dout.flush();
+            dout.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+    }
+}
+
+import java.io.*;
+import java.net.*;
+public class Myserver {
+        public static void main(String[] args){
+            try {
+                ServerSocket ss = new ServerSocket(6666);
+                Socket s = ss.accept();
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                String str = (String)dis.readUTF();
+                System.out.println("messageeeee = "+str);
+                ss.close();
+
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }    
+}
